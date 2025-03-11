d468b06e9f821050a5fe619b2c2b76aaa10f9594
Sarorian <shamowen5@gmail.com>
2025-03-11 13:56:04 -0600

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import PlayersPage from "./pages/PlayersPage/PlayersPage";
 import MatchDetails from "./pages/MatchDetails";
-import PlayerProfile from "./pages//PlayersPage/PlayerProfile"; // Import Player Profile
+import PlayerProfile from "./pages/PlayersPage/PlayerProfile"; // Import Player Profile
 
 function App() {
   return (
@@ -14,10 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/players" element={<PlayersPage />} />
-          <Route path="/players/:id" element={<PlayerProfile />} />{" "}
-          <Route path="/matches/:matchId" element={<MatchDetails />} />{" "}
-          {/* Add this route */}
-          {/* Uses Riot ID */}
+          <Route path="/players/:id" element={<PlayerProfile />} />
+          <Route path="/matches/:matchId" element={<MatchDetails />} />
         </Routes>
       </div>
     </Router>
